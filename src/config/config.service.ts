e4e8f2d82e7a8b844f1b3ec9b97b97533bcf607c
Brandon Rincon <bs.rincon@uniandes.edu.co>
2020-01-07 11:55:54 -0500

    change reference database

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -32,7 +32,7 @@ class ConfigService {
 
   public getTypeOrmConfig(): TypeOrmModuleOptions {
     return {
-      type: 'postgres',
+      type: 'mysql',
       host: this.getValue('POSTGRES_HOST'),
       port: parseInt(this.getValue('POSTGRES_PORT')),
       username: this.getValue('POSTGRES_USER'),
